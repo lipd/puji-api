b7531a941888e63586250ce924729f67637d8eac
lipd <lipeidao@gmail.com>
2021-05-08 13:48:04 +0800

    feat: 配置错误处理中间件

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
+const error = require('koa-json-error')
 const routing = require('./routes')
 
 const app = new Koa()
+app.use(
+  error({
+    postFormat: (err, { stack, ...rest }) =>
+      process.env.NODE_ENV === 'production' ? rest : { stack, rest },
+  }),
+)
 app.use(bodyParser())
 
 routing(app)
