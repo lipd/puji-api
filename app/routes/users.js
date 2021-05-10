624377d805c1b40442b908a86325b599961cca82
lipd <lipeidao@gmail.com>
2021-05-11 00:13:13 +0800

    feat: 验证权限

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,7 +1,18 @@
+const jwt = require('koa-jwt')
 const Router = require('koa-router')
 const controller = require('../controllers/users')
+const { secret } = require('../config')
 
 const router = new Router({ prefix: '/users' })
+
+const auth = jwt({ secret })
+
+const checkOwner = async (ctx, next) => {
+  if (ctx.params.id !== ctx.state.user._id) {
+    ctx.throw(403, '没有权限')
+  }
+  await next()
+}
 
 router.get('/', controller.find)
 
@@ -9,8 +20,8 @@ router.get('/:id', controller.findById)
 
 router.post('/', controller.create)
 
-router.patch('/:id', controller.update)
+router.patch('/:id', auth, checkOwner, controller.update)
 
-router.delete('/:id', controller.delete)
+router.delete('/:id', auth, checkOwner, controller.delete)
 
 module.exports = router
