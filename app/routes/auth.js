84f7dde92e77d78a15414168329c1f1960f19fce
lipd <lipeidao@gmail.com>
2021-05-14 08:27:59 +0800

    refactor: 将 auth 和 checkOwner 封装为中间件

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,8 +1,10 @@
 const Router = require('koa-router')
 const controller = require('../controllers/auth')
+const userController = require('../controllers/users')
 
 const router = new Router()
 
 router.post('/login', controller.login)
+router.post('/register', userController.create, controller.register)
 
 module.exports = router
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,24 +1,14 @@
-const jwt = require('koa-jwt')
 const Router = require('koa-router')
 const controller = require('../controllers/users')
-const { secret } = require('../config')
+const { auth, checkOwner } = require('../middleware/auth')
 
 const router = new Router({ prefix: '/users' })
-
-const auth = jwt({ secret })
-
-const checkOwner = async (ctx, next) => {
-  if (ctx.params.id !== ctx.state.user._id) {
-    ctx.throw(403, '没有权限')
-  }
-  await next()
-}
 
 router.get('/', controller.find)
 
 router.get('/:id', controller.findById)
 
-router.post('/', controller.create)
+// router.post('/', controller.create)
 
 router.patch('/:id', auth, checkOwner, controller.update)
 
