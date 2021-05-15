8153a24278797ff41201d8ed9a647eae822a2d46
lipd <lipeidao@gmail.com>
2021-05-15 08:38:03 +0800

    feat: 实现 /me 路由，完成登录返回参数

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -11,15 +11,21 @@ class ScoresController {
     })
     const user = await User.findOne(ctx.request.body)
     if (!user) ctx.throw(401, '用户名或密码不正确')
-    const { _id } = user
+    const { _id, name, avatar } = user
     const token = jwt.sign({ _id }, secret, { expiresIn: '3d' })
-    ctx.body = { token }
+    ctx.body = { token, _id, name, avatar }
   }
 
   async register(ctx) {
     const { _id, name, avatar } = ctx.body
     const token = jwt.sign({ _id }, secret, { expiresIn: '3d' })
     ctx.body = { _id, name, avatar, token }
+  }
+
+  async me(ctx) {
+    const user = await User.findById(ctx.state.user._id)
+    if (!user) ctx.throw(401, '令牌失效')
+    ctx.body = user
   }
 }
 
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,10 +1,12 @@
 const Router = require('koa-router')
 const controller = require('../controllers/auth')
 const userController = require('../controllers/users')
+const { auth } = require('../middleware/auth')
 
 const router = new Router()
 
 router.post('/login', controller.login)
 router.post('/register', userController.create, controller.register)
+router.post('/me', auth, controller.me)
 
 module.exports = router
